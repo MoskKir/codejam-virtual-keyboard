893c7125cf4d67b878aa7239309d3eae21b4bff2
Kirill Moskalevich <mosk9364@gmail.com>
2019-11-04 16:17:26 +0300

    feat: add switch language

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import './scss/style.scss';
 
-const keysArray = [
+const keysArrayEn = [
     [
         '`',
         '1',
@@ -76,51 +76,135 @@ const keysArray = [
         '►'
     ]
 ];
-
+const keysArrayRu = [
+    [
+        '`',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '0',
+        '-',
+        '=',
+        'Backspace'
+    ],
+    [
+        'Tab',
+        'й',
+        'ц',
+        'у',
+        'к',
+        'е',
+        'н',
+        'г',
+        'ш',
+        'щ',
+        'з',
+        'х',
+        'ъ',
+        '\\'
+    ],
+    [
+        'CapsLK',
+        'ф',
+        'ы',
+        'в',
+        'а',
+        'п',
+        'р',
+        'о',
+        'л',
+        'д',
+        'ж',
+        "э",
+        'Enter'
+    ],
+    [
+        'Shift',
+        'я',
+        'ч',
+        'с',
+        'м',
+        'и',
+        'т',
+        'ь',
+        'б',
+        'ю',
+        '.',
+        'Shift',
+        '▲',
+        '4'
+    ],
+    [
+        'Ctrl',
+        'Win',
+        'Alt',
+        '',
+        'Alt',
+        'Ctrl',
+        '◄',
+        '▼',
+        '►'
+    ]
+];
 
 let container = document.createElement('div'),
     keyboard = document.createElement('div'),
     textArea = document.createElement('textarea');
 
-container.classList.add('container');
-
-document.body.appendChild(container);
-
-textArea.classList.add(
-'text-area',
-'container__text-area'
-);
-
-container.appendChild(textArea);
-textArea.focus();
-
-keyboard.classList.add('keyboard');
-container.appendChild(keyboard);
-
-for(let t = 0; t<keysArray.length; t++){
-
-    let row = document.createElement('div');
-
-    row.classList.add('row', `row${t}`);
+    container.classList.add('container');
     
-    keyboard.append(row);
+    document.body.appendChild(container);
+    
+    textArea.classList.add('text-area', 'container__text-area');
+    container.appendChild(textArea);
 
-    for (let i = 0; i<keysArray[t].length; i++){
-        const key = document.createElement('div');
+    keyboard.classList.add('keyboard', 'En');
 
-        key.classList.add('key', `key_${keysArray[t][i]}`);
-        row.appendChild(key);
-        key.textContent = keysArray[t][i];
-        
+let renderVirtualKeybord = (keysArray) => {
+    
+
+    while (keyboard.firstChild) {
+        keyboard.removeChild(keyboard.firstChild);
     }
 
+
+    textArea.focus();
+    
+    container.appendChild(keyboard);
+    
+    for(let t = 0; t < keysArray.length; t++) {
+    
+        let row = document.createElement('div');
+    
+        row.classList.add('row', `row${t}`);
+        
+        keyboard.append(row);
+    
+        for (let i = 0; i<keysArray[t].length; i++){
+            const key = document.createElement('div');
+    
+            key.classList.add('key', `Key_${keysArray[t][i]}`);
+            row.appendChild(key);
+            key.textContent = keysArray[t][i];
+            
+        }
+    
+    }
 }
+renderVirtualKeybord(keysArrayEn);
 
 document.body.addEventListener('keydown', (event) => {
     
     console.log(event.key);
-
-    let key = document.querySelector(`.key_${event.key}`);
+    console.log(event.code);
+    setTimeout(() => textArea.focus(), 100);
+    let key = document.querySelector(`.Key_${event.key}`);
    
     console.log(key);
 
@@ -132,7 +216,54 @@ keyboard.addEventListener('click', (event) => {
     console.log(event.target.textContent);
     textArea.value += event.target.textContent;
 
-    let key = document.querySelector(`.key_${event.target.textContent}`);
+    let key = document.querySelector(`.Key_${event.target.textContent}`);
     key.classList.add('keyActive');
     setTimeout(() => key.classList.remove('keyActive'), 100);
-})
+    // textArea.focus();
+    
+});
+
+function runOnKeys(func, ...codes) {
+    let pressed = new Set();
+
+    document.addEventListener('keydown', function(event) {
+      pressed.add(event.code);
+
+      for (let code of codes) {
+        if (!pressed.has(code)) {
+          return;
+        }
+      }
+      pressed.clear();
+
+      func();
+    });
+
+    document.addEventListener('keyup', function(event) {
+      pressed.delete(event.code);
+    });
+
+}
+
+runOnKeys(
+    () => {
+        console.log('runOnKeys')
+        if (keyboard.classList.contains('En')) {
+
+            console.log('var1')
+            renderVirtualKeybord(keysArrayRu);
+            keyboard.classList.remove('En');
+            keyboard.classList.add('Ru');
+
+        }else if (keyboard.classList.contains('Ru')) {
+
+            console.log('var2')
+            renderVirtualKeybord(keysArrayEn); 
+            keyboard.classList.remove('Ru');
+            keyboard.classList.add('En');
+
+        }
+    },
+    'ShiftLeft',
+    'AltLeft'
+);
