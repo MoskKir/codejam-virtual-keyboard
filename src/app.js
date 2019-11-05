5abb8f98df6086d99d7d649338fc031f83642d6a
Kirill Moskalevich <mosk9364@gmail.com>
2019-11-06 02:45:58 +0300

    feat: add Shift

--- src/app.js
+++ src/app.js
@@ -60,9 +60,8 @@ const keysArrayEn = [
         ',',
         '.',
         '/',
-        'Shift',
         '▲',
-        '4'
+        'Shift'
     ],
     [
         'Ctrl',
@@ -70,15 +69,166 @@ const keysArrayEn = [
         'Alt',
         '',
         'Alt',
-        'Ctrl',
         '◄',
         '▼',
-        '►'
+        '►',
+        'Ctrl'
     ]
 ];
-const keysArrayRu = [
+const keysArrayEnShift = [
+    [
+        '~',
+        '!',
+        '@',
+        '#',
+        '$',
+        '%',
+        '^',
+        '&',
+        '*',
+        '(',
+        ')',
+        '_',
+        '+',
+        'Backspace'
+    ],
+    [
+        'Tab',
+        'Q',
+        'W',
+        'E',
+        'R',
+        'T',
+        'Y',
+        'U',
+        'I',
+        'O',
+        'P',
+        '{',
+        '}',
+        '|'
+    ],
+    [
+        'CapsLK',
+        'A',
+        'S',
+        'D',
+        'F',
+        'G',
+        'H',
+        'J',
+        'K',
+        'L',
+        ':',
+        '"',
+        'Enter'
+    ],
+    [
+        'Shift',
+        'Z',
+        'X',
+        'C',
+        'V',
+        'B',
+        'N',
+        'M',
+        '<',
+        '>',
+        '?',        
+        '▲',
+        'Shift'
+    ],
+    [
+        'Ctrl',
+        'Win',
+        'Alt',
+        '',
+        'Alt',    
+        '◄',
+        '▼',
+        '►',
+        'Ctrl'
+    ]
+];
+const keysArrayEnCapsLK = [
     [
         '`',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '0',
+        '-',
+        '=',
+        'Backspace'
+    ],
+    [
+        'Tab',
+        'Q',
+        'W',
+        'E',
+        'R',
+        'T',
+        'Y',
+        'U',
+        'I',
+        'O',
+        'P',
+        '{',
+        '}',
+        '|'
+    ],
+    [
+        'CapsLK',
+        'A',
+        'S',
+        'D',
+        'F',
+        'G',
+        'H',
+        'J',
+        'K',
+        'L',
+        ':',
+        '"',
+        'Enter'
+    ],
+    [
+        'Shift',
+        'Z',
+        'X',
+        'C',
+        'V',
+        'B',
+        'N',
+        'M',
+        '<',
+        '>',
+        '?',        
+        '▲',
+        'Shift'
+    ],
+    [
+        'Ctrl',
+        'Win',
+        'Alt',
+        '',
+        'Alt',    
+        '◄',
+        '▼',
+        '►',
+        'Ctrl'
+    ]
+];
+
+const keysArrayRu = [
+    [
+        'ё',
         '1',
         '2',
         '3',
@@ -135,21 +285,170 @@ const keysArrayRu = [
         'ь',
         'б',
         'ю',
-        '.',
-        'Shift',
+        '.',        
         '▲',
-        '4'
+        'Shift'
     ],
     [
         'Ctrl',
         'Win',
         'Alt',
         '',
-        'Alt',
-        'Ctrl',
+        'Alt',        
         '◄',
         '▼',
-        '►'
+        '►',
+        'Ctrl'
+    ]
+];
+const keysArrayRuShift = [
+    [
+        'Ё',
+        '!',
+        '"',
+        '№',
+        ';',
+        '%',
+        ':',
+        '?',
+        '*',
+        '(',
+        ')',
+        '_',
+        '+',
+        'Backspace'
+    ],
+    [
+        'Tab',
+        'Й',
+        'Ц',
+        'У',
+        'К',
+        'Е',
+        'Н',
+        'Г',
+        'Ш',
+        'Щ',
+        'З',
+        'Х',
+        'Ъ',
+        '/'
+    ],
+    [
+        'CapsLK',
+        'Ф',
+        'Ы',
+        'В',
+        'А',
+        'П',
+        'Р',
+        'О',
+        'Л',
+        'Д',
+        'Ж',
+        "Э",
+        'Enter'
+    ],
+    [
+        'Shift',
+        'Я',
+        'Ч',
+        'С',
+        'М',
+        'И',
+        'Т',
+        'Ь',
+        'Б',
+        'Ю',
+        ',',        
+        '▲',
+        'Shift'
+    ],
+    [
+        'Ctrl',
+        'Win',
+        'Alt',
+        '',
+        'Alt',        
+        '◄',
+        '▼',
+        '►',
+        'Ctrl'
+    ]
+];
+const keysArrayRuCapsLK = [
+    [
+        'Ё',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '0',
+        '-',
+        '=',
+        'Backspace'
+    ],
+    [
+        'Tab',
+        'Й',
+        'Ц',
+        'У',
+        'К',
+        'Е',
+        'Н',
+        'Г',
+        'Ш',
+        'Щ',
+        'З',
+        'Х',
+        'Ъ',
+        '/'
+    ],
+    [
+        'CapsLK',
+        'Ф',
+        'Ы',
+        'В',
+        'А',
+        'П',
+        'Р',
+        'О',
+        'Л',
+        'Д',
+        'Ж',
+        "Э",
+        'Enter'
+    ],
+    [
+        'Shift',
+        'Я',
+        'Ч',
+        'С',
+        'М',
+        'И',
+        'Т',
+        'Ь',
+        'Б',
+        'Ю',
+        ',',        
+        '▲',
+        'Shift'
+    ],
+    [
+        'Ctrl',
+        'Win',
+        'Alt',
+        '',
+        'Alt',        
+        '◄',
+        '▼',
+        '►',
+        'Ctrl'
     ]
 ];
 
@@ -157,76 +456,171 @@ let container = document.createElement('div'),
     keyboard = document.createElement('div'),
     textArea = document.createElement('textarea');
 
-    container.classList.add('container');
+textArea.rows = 20;
+textArea.cols = 30;
+container.classList.add('container');
     
-    document.body.appendChild(container);
-    
-    textArea.classList.add('text-area', 'container__text-area');
-    container.appendChild(textArea);
+document.body.appendChild(container);
 
-    keyboard.classList.add('keyboard', 'En');
-
-let renderVirtualKeybord = (keysArray) => {
     
+textArea.classList.add('text-area');
+container.appendChild(textArea);
+    
+keyboard.classList.add('keyboard', 'En');
+
+
+let capsLockSwitcher = 'off';
+let shiftSwitcher = 'off';
+let languageSwicher = 'en';
+
+
+    
+let renderVirtualKeybord = (keysArray) => {   
 
     while (keyboard.firstChild) {
         keyboard.removeChild(keyboard.firstChild);
     }
-
-
+    
     textArea.focus();
     
     container.appendChild(keyboard);
     
     for(let t = 0; t < keysArray.length; t++) {
-    
+        
         let row = document.createElement('div');
-    
+        
         row.classList.add('row', `row${t}`);
         
         keyboard.append(row);
-    
-        for (let i = 0; i<keysArray[t].length; i++){
-            const key = document.createElement('div');
-    
+        
+        for (let i = 0; i < keysArray[t].length; i++){
+            let key = document.createElement('div');
+            
             key.classList.add('key', `Key_${keysArray[t][i]}`);
             row.appendChild(key);
+
+           
+
             key.textContent = keysArray[t][i];
+
+            key.addEventListener('click', (event) => {
+
+                let keyTextContent = event.target.textContent;
+                
+                event.target.classList.add('keyActive');
+                setTimeout(() => event.target.classList.remove('keyActive'), 100);
+
+                if (keyTextContent === '') {
+                    return textArea.value += ' ';
+                } else if (keyTextContent === 'Tab') {
+                    return textArea.value += '    ';
+
+                } else if (keyTextContent === 'Enter') {
+                    return textArea.value += '\n';
+
+                } else if (keyTextContent === 'Ctrl') {
+                    return textArea.value += '';
+                } else if (keyTextContent === 'Alt') {
+                    return textArea.value += '';
+                } else if (keyTextContent === 'CapsLK') {
+                    if (capsLockSwitcher === 'off') {
+                        capsLockSwitcher = 'on';
+                        if (languageSwicher === 'en') {
+                            return renderVirtualKeybord(keysArrayEnCapsLK);
+                        } else {
+                            return renderVirtualKeybord(keysArrayRuCapsLK);
+                        }                        
+                    } else {
+                        capsLockSwitcher = 'off';
+                        if (languageSwicher === 'en') {
+                            return renderVirtualKeybord(keysArrayEn);
+                        } else {
+                            return renderVirtualKeybord(keysArrayRu);
+                        }
+                    }
+                } else if (keyTextContent === 'Shift') { 
+
+                    if (shiftSwitcher === 'off') {
+                        shiftSwitcher = 'on';
+                        if (languageSwicher === 'en') {
+                            return renderVirtualKeybord(keysArrayEnShift);
+                        } else {
+                            return renderVirtualKeybord(keysArrayRuShift);
+                        }
+                    } else {
+                        shiftSwitcher = 'off';
+                        if (languageSwicher === 'en') {
+                            return renderVirtualKeybord(keysArrayEn);
+                        } else {
+                            return renderVirtualKeybord(keysArrayRu);
+                        }
+                    }
+
+
+                } else if (keyTextContent === 'Win') {
+                    return textArea.value += '';
+                } else if (keyTextContent === 'Backspace') {
+                    return textArea.value = textArea.value.slice(0, textArea.value.length - 1);
+                }
+
+                
+
+                textArea.value += event.target.textContent;
+            })
             
         }
-    
-    }
+    }  
 }
-renderVirtualKeybord(keysArrayEn);
+
+
+let loadLanguage = () => {
+    // keyCode: 83 key: s или ы
+
+    document.body.addEventListener('keydown', (event) => {
+        // console.log(event)
+        // console.log(event.KeyboardEvent)
+        
+        // console.log(event.code)
+        // console.log(event.key)
+        let chekLanguage = `Key${event.key.toUpperCase()}`;
+        
+        
+        if (event.code === chekLanguage ) {
+            renderVirtualKeybord(keysArrayEn);
+        } else {
+            renderVirtualKeybord(keysArrayRu);
+        }
+    })
+    
+};
+window.onload = loadLanguage();
+
+
 
 document.body.addEventListener('keydown', (event) => {
-    
-    console.log(event.key);
-    console.log(event.code);
-    setTimeout(() => textArea.focus(), 100);
+    // console.log(event)
+    // console.log(event.key);
+    // console.log(event.code);
+    // console.log(event.shiftKey)
+
     let key = document.querySelector(`.Key_${event.key}`);
-   
-    console.log(key);
 
-    key.classList.add('keyActive');
-    setTimeout(() => key.classList.remove('keyActive'), 100);
-});
-
-keyboard.addEventListener('click', (event) => {
-    console.log(event.target.textContent);
-    textArea.value += event.target.textContent;
-
-    let key = document.querySelector(`.Key_${event.target.textContent}`);
-    key.classList.add('keyActive');
-    setTimeout(() => key.classList.remove('keyActive'), 100);
-    // textArea.focus();
+    if (event.key === 'Shift') {
+        key.classList.add('keyActive');
+    } else {
+        setTimeout(() => textArea.focus(), 100);
     
+        key.classList.add('keyActive');
+        setTimeout(() => key.classList.remove('keyActive'), 100);
+    }
+
 });
 
-function runOnKeys(func, ...codes) {
+// Этот работает нормально
+let runOnKeys = (func, ...codes) => {
     let pressed = new Set();
 
-    document.addEventListener('keydown', function(event) {
+    document.addEventListener('keydown', event => {
       pressed.add(event.code);
 
       for (let code of codes) {
@@ -239,7 +633,7 @@ function runOnKeys(func, ...codes) {
       func();
     });
 
-    document.addEventListener('keyup', function(event) {
+    document.addEventListener('keyup', event => {
       pressed.delete(event.code);
     });
 
@@ -247,17 +641,13 @@ function runOnKeys(func, ...codes) {
 
 runOnKeys(
     () => {
-        console.log('runOnKeys')
+        
         if (keyboard.classList.contains('En')) {
-
-            console.log('var1')
             renderVirtualKeybord(keysArrayRu);
             keyboard.classList.remove('En');
             keyboard.classList.add('Ru');
 
-        }else if (keyboard.classList.contains('Ru')) {
-
-            console.log('var2')
+        } else if (keyboard.classList.contains('Ru')) {
             renderVirtualKeybord(keysArrayEn); 
             keyboard.classList.remove('Ru');
             keyboard.classList.add('En');
@@ -267,3 +657,33 @@ runOnKeys(
     'ShiftLeft',
     'AltLeft'
 );
+
+let choiseLanguage = () => {
+
+    let ruLanguage = document.createElement('div'),
+        enLanguage = document.createElement('div'),
+        text = document.createElement('div');
+    
+    
+    
+    text.innerHTML = `Please put on any button on you keybord or choise with display`;
+    
+    ruLanguage.innerHTML = `Русский`;
+    enLanguage.innerHTML = `Engles`;
+        
+    container.appendChild(text);
+    text.appendChild(ruLanguage);
+    text.appendChild(enLanguage);
+    
+    ruLanguage.addEventListener('click', () => {
+        languageSwicher = 'ru'; 
+        renderVirtualKeybord(keysArrayRu);
+    });
+        
+    enLanguage.addEventListener('click', () => {
+        languageSwicher = 'en'; 
+        renderVirtualKeybord(keysArrayEn);
+    });
+}
+
+choiseLanguage();
